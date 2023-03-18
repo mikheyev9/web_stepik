b6558af65eece48aee391eb5ed1fdc0168efa3a6
mikheyev9 <mikheyev9@gmail.com>
2023-03-18 13:38:58 +0300

    f

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
-sudo ln -s  /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm -fr /etc/nginx/sites-available/default
+sudo ln -sf  /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
